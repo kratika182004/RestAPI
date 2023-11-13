a1024e3ba3be529fd974ed06250aaa27c4f899e0
Kratika Gupta <kratikagupta@Kratikas-MacBook-Air.local>
2023-11-13 21:24:11 +0530

    Add new LoginSignUp project

---
+++ src/db/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://0.0.0.0:27017/user", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log("Connection successful");
+    })
+    .catch(err => {
+        console.error("Error connecting to the database:", err);
+    });
---
+++ src/modl/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: {
+     type: String,
+     require: true,
+     trim: true,
+     unique: true,
+     lowercase: true,
+     index: true,
+  },
+  email: {
+     type: String,
+     require: true,
+     trim: true,
+     unique: true,
+     lowercase: true,
+  },
+  password: {
+     type: String,
+     require: true,
+  },
+},{ timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
+
+
+
